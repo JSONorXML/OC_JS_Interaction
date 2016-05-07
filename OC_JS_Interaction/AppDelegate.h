783fc6fb178e29e2c73dd70d9ca3e350eace81ff
李长青 <lichangqing@lichangqingdeimac.local>
2016-05-07 09:53:46 +0800

    Initial Commit

---
+++ OC_JS_Interaction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_JS_Interaction
+//
+//  Created by 李长青 on 16/5/7.
+//  Copyright © 2016年 LiChangqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
